705c3a3ca2214e5d60793738a835aa70afa0b2ec
PabloCarrai <pablohernanjuan@hotmail.com>
2026-02-14 11:57:56 -0300

    Agregando archivo de ejemplo del archivo original .env. Agregamos modulo buchon que es quien hace la parte de enviar mensaje a telegram. Y los requirements que son los modulos que vamos a usar en nuestro entorno virtual

---
+++ app.py
@@ -0,0 +1,8 @@
+from .buchon import enviarMensajes as em
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ buchon.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+#   Cargo las variables del archivo .env
+load_dotenv()
+
+
+def enviarMensajes(token, chat, mensaje):
+    """
+    enviarMensajes; Envia mensaje a bot de telegrams
+
+    :param token: Tu token de telegram
+    :param chat: Tu id de chat de telegram
+    :param mensaje: Tu mensaje a enviar
+    """
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    parametros = {"chat": chat, "mensaje": mensaje, "parse_mode": "Markdown"}
+    try:
+        response = requests.get(url, params=parametros)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Ocurrio un error: {e}")
+        return None
+
+
+#  Asigno los valores de las variables del .env a una variable interna
+token = os.environ.get("TOKEN")
+chat = os.environ.get("CHAT")
